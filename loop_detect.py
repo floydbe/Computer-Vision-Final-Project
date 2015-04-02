51ebe6e4a802ea02bd8f7491052d7c57751dd35a
Will Hawkins <hawkinsw@opentechinstitute.org>
2015-04-01 21:32:17 -0400

    Actually do loop detection!

--- loop_detect.py
+++ loop_detect.py
@@ -5,3 +5,14 @@ import numpy as np
 import Image
 from video import Video
 from distance import d
+
+def Match():
+	v = Video("centaur_1.mpg")
+
+	for f,i in zip(v, range(len(v)-1)):
+		print("i: %d" % i)
+		for g,j in zip(v.sub_iter(i), range(len(v))):
+			print("d(%d,%d) = %f" % (i, i+j, d(f,g)))
+
+if __name__== "__main__":
+	Match()
