837dc09add29a1a140f10a875a85f6b17c25b530
Will Hawkins <hawkinsw@opentechinstitute.org>
2015-04-03 00:09:06 -0400

    Move testing into if-protected section.

--- make_gif.py
+++ make_gif.py
@@ -3,4 +3,5 @@ from scitools.std import movie
 def make_gif(frame_loc, frames_per_sec=1, output_file_loc="my_gif.gif"):
 	movie(frame_loc + "/*.jpg", fps=frames_per_sec, output_file=output_file_loc)
 
-make_gif("frames", 2)
+if __name__=="__main__":
+	make_gif("frames", 2)
