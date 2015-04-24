d46540bba7d7d55c816db5b8e5531de8f1030a1a
Will Hawkins <hawkinsw@opentechinstitute.org>
2015-04-24 01:28:25 -0400

    New handling for min/max length parameters.
    1. Make it so that the user can specify both
    a minimum and maximum loop length
    2. Make it so that the user can specify it
    in frames or seconds.

--- loop_detect.py
+++ loop_detect.py
@@ -11,7 +11,7 @@ import re
 from debug import Debug
 from operator import itemgetter
 
-def Match(v, start_frame, end_frame, max_span = None):
+def Match(v, start_frame, end_frame, min_span = 0, max_span = None):
 	matches = []
 
 	# if max_span was not user-supplied,
@@ -23,11 +23,11 @@ def Match(v, start_frame, end_frame, max_span = None):
 	for f,i in v.sub_iter(start_frame):
 		if i > end_frame:
 			break
-		for g,j in v.sub_iter(i):
+		for g,j in v.sub_iter(i + min_span):
 			distance = 0.0
 			# if we are calculating a distance that is longer than the
 			# max_span, move on.
-			if (j-i) >= max_span:
+			if (j-i) > max_span:
 				break
 			# don't calculate the distance to ourselves.
 			if i == j:
@@ -38,18 +38,20 @@ def Match(v, start_frame, end_frame, max_span = None):
 	return matches
 
 def usage(myname):
-	print("""%s 
-	[-s, --start <start frame>]
-	[-e, --end <end frame> ] 
-	[-l, --length <maximum loop length> ]
-	[-o, --optimize [edge, scale, all] ]
-	[-t, --threshold <threshold> (only applicable in combination with -o)]
-	<input video filename> <output animated gif filename>""" % myname)
+	print("""%s
+[-s, --start <start frame>]
+[-e, --end <end frame> ]
+[-l, --min-length <minimum loop length (# for frames or #.#s for seconds)> ]
+[-m, --max-length <minimum loop length (# for frames or #.#s for seconds)> ]
+[-o, --optimize [edge, scale, all] ]
+[-t, --threshold <threshold> (only applicable in combination with -o)]
+<input video filename> <output animated gif filename>""" % myname)
 
 if __name__== "__main__":
 	start_frame = 0
 	end_frame = None
 	max_length = None
+	min_length = 0
 	video = None
 	input_filename = None
 	output_filename = None
@@ -58,7 +60,7 @@ if __name__== "__main__":
 	do_scale_match_optimization = False
 
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "s:e:o:t:l:", ["start=", "end=", "optimize=", "threshold=", "length="])
+		opts, args = getopt.getopt(sys.argv[1:], "s:e:o:t:l:m:", ["start=", "end=", "optimize=", "threshold=", "min-length=", "max-length="])
 
 		if len(args) != 2:
 			raise getopt.GetoptError("Must specify input and output filenames as parameters.")
@@ -71,8 +73,10 @@ if __name__== "__main__":
 				start_frame = int(a)
 			elif o == "e" or o == "end":
 				end_frame = int(a)
-			elif o == "l" or o == "length":
-				max_length = int(a)
+			elif o == "l" or o == "min-length":
+				min_length = a
+			elif o == "m" or o == "max-length":
+				max_length = a
 			elif o == "o" or o == "optimize":
 				if a == "edge":
 					do_edge_match_optimization = True
@@ -93,12 +97,44 @@ if __name__== "__main__":
 	if end_frame == None:
 		end_frame = len(video)
 
+	# Convert length parameters with s variables to frames.
+	try:
+		if max_length != None:
+			if str(max_length)[-1] == 's':
+				max_length = int(float(max_length[:-1])*video.frames_per_second)
+			else:
+				max_length = int(max_length)
+	except ValueError:
+		print("Warning: Max length parameter is not valid. Using default.\n")
+		max_length = None
+	Debug.Print("max_length: %s\n" % str(max_length))
+
+	try:
+		if str(min_length)[-1] == 's':
+			min_length = int(float(min_length[:-1])*video.frames_per_second)
+		else:
+			min_length = int(min_length)
+	except ValueError:
+		print("Warning: Min length parameter is not valid. Using default.\n")
+		min_length = 0
+	Debug.Print("min_length: %d\n" % min_length)
+
+	# Sanitize user inputs
+	if max_length != None and max_length < min_length:
+		print("Max length cannot be shorter than min length.")
+		usage(sys.argv[0])
+		sys.exit(1)
+
 	# matches is a list of tuples: (start frame, end frame, distance)
 	matches = []
 	if do_edge_match_optimization and not do_scale_match_optimization:
 		# Edge optimization but no scale optimization
 		edge_video = EdgeVideo(video)
-		edge_matches = Match(edge_video, start_frame, end_frame, max_length)
+		edge_matches = Match(edge_video,
+			start_frame,
+			end_frame,
+			min_length,
+			max_length)
 		edge_matches = sorted(edge_matches, key=itemgetter(2))
 		# compute full matches for good edge matches
 		for a,b,distance in edge_matches:
@@ -107,7 +143,11 @@ if __name__== "__main__":
 	elif do_scale_match_optimization and not do_edge_match_optimization:
 		# Scale optimization but no edge optimization
 		scale_video = ScaleVideo(video)
-		scale_matches = Match(scale_video, start_frame, end_frame, max_length)
+		scale_matches = Match(scale_video,
+			start_frame,
+			end_frame,
+			min_length,
+			max_length)
 		scale_matches = sorted(scale_matches, key=itemgetter(2))
 		# compute full matches for good scale matches
 		for a,b,distance in scale_matches:
@@ -117,21 +157,25 @@ if __name__== "__main__":
 		# Both optimizations
 		edge_video = EdgeVideo(video)
 		scale_video = ScaleVideo(edge_video)
-		scale_matches = Match(scale_video, start_frame, end_frame, max_length)
+		scale_matches = Match(scale_video,
+			start_frame,
+			end_frame,
+			min_length,
+			max_length)
 		scale_matches = sorted(scale_matches, key=itemgetter(2))
 		# compute full matches for good scale matches
 		for a,b,distance in scale_matches:
 			if distance < threshold:
 				matches.append((a,b,d(video[a],video[b],video.norms[a],video.norms[b])))
 	else:
-		matches = Match(video, start_frame, end_frame, max_length)
+		matches = Match(video, start_frame, end_frame, min_length, max_length)
 
 	# Sort the matches according to their distance values
 	# which is the 2th index of items in matches.
 	matches = sorted(matches, key=itemgetter(2))
 
-	# make matches a minimum distance of three frames.
-	matches = [ match for match in matches if match[1]-match[0] >= 9 ]
+	# make matches a minimum distance
+	matches = [ match for match in matches if match[1]-match[0] >= min_length ]
 
 	# print matches in csv style.
 	Debug.Print("Results: \n")
